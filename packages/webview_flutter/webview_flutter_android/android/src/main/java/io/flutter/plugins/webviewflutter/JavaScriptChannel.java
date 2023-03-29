b8273138d67ebf8d33731b3210a64cb995fe656b
s-sugita <s_sugita@mkb.ne.jp>
2023-03-29 09:37:40 +0900

    mkbプロジェクトのためにAndroid用のメソッド準備

--- packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/JavaScriptChannel.java
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/JavaScriptChannel.java
@@ -3,7 +3,8 @@
 // found in the LICENSE file.
 
 package io.flutter.plugins.webviewflutter;
-
+// 仕方なく追加
+import android.util.Log;
 import android.os.Handler;
 import android.os.Looper;
 import android.webkit.JavascriptInterface;
@@ -40,11 +41,100 @@ public class JavaScriptChannel {
   // Suppressing unused warning as this is invoked from JavaScript.
   @SuppressWarnings("unused")
   @JavascriptInterface
+  // これはつかわない
   public void postMessage(final String message) {
     final Runnable postMessageRunnable =
         () -> {
           flutterApi.postMessage(JavaScriptChannel.this, message, reply -> {});
         };
+
+    if (platformThreadHandler.getLooper() == Looper.myLooper()) {
+      postMessageRunnable.run();
+    } else {
+      platformThreadHandler.post(postMessageRunnable);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  @JavascriptInterface
+  public void init(final String message) {
+    finalMessage("init@" + message);
+  }
+
+  @SuppressWarnings("unused")
+  @JavascriptInterface
+  public void getValue(final String message) {
+    finalMessage("getValue@" + message);
+  }
+
+  @SuppressWarnings("unused")
+  @JavascriptInterface
+  public void setValue(final String message) {
+    finalMessage("setValue@" + message);
+  }
+
+  @SuppressWarnings("unused")
+  @JavascriptInterface
+  public void deleteValue(final String message) {
+    finalMessage("deleteValue@" + message);
+  }
+
+  @SuppressWarnings("unused")
+  @JavascriptInterface
+  public void insertArchive(final String message) {
+    finalMessage("insertArchive@" + message);
+  }
+
+
+  /*
+   *   static const apiGetArchiveCount = 'getArchiveCount';
+
+  static const apiSelectProfile = 'selectProfile';
+  static const apiOpenArchive = 'openArchive';
+  static const apiGetArchive = 'getArchive';
+  // TODO: 今まで使ったことがないので不要になります
+  static const apiOpenNotify = 'openNotify';
+  static const apiOpenTextField = 'openTextField';
+  static const apiOpenDatePicker = 'openDatePicker';
+  // TODO: 今まで使ったことがないので不要になります
+  static const apiOpenTimePicker = 'openTimePicker';
+  // TODO: 今まで使ったことがないので不要になります
+  static const apiOpenSelectPicker = 'openSelectPicker';
+  // TODO: 今まで使ったことがないので不要になります
+  static const apiUpdateProfile = 'updateProfile';
+
+   */
+
+  @SuppressWarnings("unused")
+  @JavascriptInterface
+  public void apiAddFavorite(final String message) {
+    finalMessage("apiAddFavorite@" + message);
+  }
+
+  /*
+   *   static const apiPay = 'pay';
+  static const apiOpenUrl = 'openUrl';
+  static const apiCheckNetwork = 'checkNetwork';
+  */
+  @SuppressWarnings("unused")
+  @JavascriptInterface
+  public void showToast(final String message) {
+    finalMessage("showToast@" + message);
+  }
+
+  /*
+  static const apiDownloadImage = 'downloadImage';
+  static const apiOpenChildApp = 'openChildApp';
+  static const apiOpenNativeApp = 'openNativeApp';
+  static const apiOpenSearch = 'openSearch';
+  static const apiExitChildApp = 'exitChildApp';
+   */
+  
+  private void finalMessage(final String message) {
+    final Runnable postMessageRunnable =
+            () -> {
+              flutterApi.postMessage(JavaScriptChannel.this, message, reply -> {});
+            };
 
     if (platformThreadHandler.getLooper() == Looper.myLooper()) {
       postMessageRunnable.run();
